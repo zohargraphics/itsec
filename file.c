a375a97a0b232bd4d009c5352398ac972a1c177c
zohargraphics <zohargraphics.info@gmail.com>
2022-09-03 16:38:43 +0300

    file.c

---
+++ file.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <openssl/bn.h>
+#define NBITS 256
+
+void printBN(char *msg, BIGNUM * a)
+{
+char * number_str = BN_bn2hex(a);
+printf("%s %s\n, msg, number_str);
+OPENSSL_free(number_str);
+}
+
+int main()
+{
+BN_CTX *ctx = BN+CTX_new();
+
+BIGNUM *p = BN_new();
+BIGNUM *q = BN_new();
+BIGNUM *e = BN_new();
+BIGNUM *n = BN_new();
+BIGNUM *d = BN_new();
+BIGNUM *res1 = BN_new();
+BIGNUM *res2 = BN_new();
+BIGNUM *one = BN_new();
+
+BN_hex2bn(&p, "F7E75FDC469067FFDC4E847C51F452DF");
+BN_hex2bn(&q, "E85CED54AF57E53E092113E62F436F4F");
+BN_hex2bn(&e, "0d88c3");
+BN_hex2bn(&one, "1");
+
+BN_sub(res1, p, one);
+BN_sub(res2, q, one);
+BN_mul(n, res1, res2, ctx);
+
+BN_mod_inverse(d, e, n, ctx);
+printBN("private key d = ", d);
+
+return 0;
+}
