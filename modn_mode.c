de5a67da972a684fa5f3e1f809c34f07b16bb5cb
ZOHAR GRAPHICS <106773469+zohargraphics@users.noreply.github.com>
2022-09-04 11:49:31 +0300

    Update modn_mode.c

--- modn_mode.c
+++ modn_mode.c
@@ -14,10 +14,10 @@ int main()
     BN_CTX *ctx = BN_CTX_new();
 
     BIGNUM *n = BN_new();
-    BIGNUM *m = BN_new();
+    BIGNUM *M = BN_new();
     BIGNUM *e = BN_new();
     BIGNUM *d = BN_new();
-    BIGNUM *c = BN_new();
+    BIGNUM *C = BN_new();
     BIGNUM *decr = BN_new();
 
     BN_hex2bn(&n, "DCBFFE3E51F62E09CE7032E2677A78946A849DC4CDDE3A4D0CB81629242FB1A5");
@@ -25,11 +25,11 @@ int main()
     BN_hex2bn(&e, "010001");
     BN_hex2bn(&d, "74D806F9F3A62BAE331FFE3F0A68AFE35B3D2E4794148AACBC26AA381CD7D30D");
 
-    BN_mod_exp(c, m, e, n, ctx);
-    printBN("m^e mod n = ",  c);
+    BN_mod_exp(C, M, e, n, ctx);
+    printBN("m^e mod n = ",  C);
     printf("\n\n");
 
-    BN_mod_exp(decr, c, d, ctx);
+    BN_mod_exp(decr, C, d, ctx);
     printBN("c^d mod n = ", decr);
 
     return 0;
